d5c0c3ae980d380ee1c72afcfc7076f4c5710486
mrister <mihovil.rister@gmail.com>
2017-07-12 14:30:11 +0200

    Init and setup project with S3 storage provider as default
    Add ability to download, upload and delete files with s3 provider and streams
    Add tests (AWS creds from environment)
    Update Readme with examples
    Update .gitignore to be like in other projects
    Add support for providers adding and fetching
    Split tests into unit and integration
    Add .env and dotenv for integration testing

---
+++ src/providers/s3.js
@@ -0,0 +1,95 @@
+const S3 = require('aws-sdk/clients/s3');
+const assert = require('assert');
+const BaseProvider = require('./base');
+
+const s3 = Symbol('s3');
+
+class S3Provider extends BaseProvider {
+  /**
+   * Creates a new s3 provider
+   * @param {Configuration} config
+   */
+  constructor(config) {
+    super(config);
+    this.name = 'S3';
+    this[s3] = new S3({
+      maxRetries: config.options.maxRetries || 1,
+      accessKeyId: config.auth.AWS_ACCESS_KEY_ID || null,
+      secretAccessKey: config.auth.AWS_SECRET_ACCESS_KEY || null,
+      region: config.auth.AWS_REGION || null
+    });
+
+    this.uploadFile = this.uploadFile.bind(this);
+    this.downloadFile = this.downloadFile.bind(this);
+    this.deleteFile = this.deleteFile.bind(this);
+  }
+
+  downloadFile(location, uploadName, writableStream) {
+    return new Promise((resolve, reject) => {
+      assert(location, 'location of the upload needs to be provided');
+      assert(uploadName, 'uploadName needs to be provided');
+      assert(writableStream, 'writableStream needs to be provided');
+
+      const opts = this.configuration.options;
+      const params = Object.assign({}, opts, {
+        Bucket: location,
+        Key: uploadName,
+      });
+
+      return this[s3]
+      .getObject(params)
+      .createReadStream()
+      .on('end', () => resolve(true))
+      .on('error', error => reject(error))
+      .pipe(writableStream);
+    });
+  }
+
+  uploadFile(location, uploadName, fileStream) {
+    return new Promise((resolve, reject) => {
+      assert(location, 'location of the upload needs to be provided');
+      assert(uploadName, 'uploadName needs to be provided');
+      assert(fileStream, 'fileStream needs to be provided');
+
+      const opts = this.configuration.options;
+      // this can take in other s3 options if provided
+      const params = Object.assign({}, opts, {
+        Bucket: location,
+        Key: uploadName,
+        Body: fileStream
+      });
+
+      const upload = this[s3].upload(params);
+
+      return upload.send((err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(result);
+      });
+    });
+  }
+
+  deleteFile(location, uploadName) {
+    return new Promise((resolve, reject) => {
+      assert(location, 'location of the upload needs to be provided');
+      assert(uploadName, 'uploadName needs to be provided');
+
+      const opts = this.configuration.options;
+      const params = Object.assign({}, opts, {
+        Bucket: location,
+        Key: uploadName,
+      });
+
+      return this[s3]
+      .deleteObject(params, (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(result);
+      });
+    });
+  }
+}
+
+module.exports = S3Provider;
---
+++ test/provider-factory.js
@@ -0,0 +1,57 @@
+const chai = require('chai');
+const fileManagement = require('../src/index');
+
+const BaseProvider = fileManagement.providers.BaseProvider;
+const expect = chai.expect;
+
+describe('Provider Factory', () => {
+  expect(fileManagement.providers).to.be.an('object').and.to.be.ok;
+
+  it('should be able to get provider names', () => {
+    expect(fileManagement.providers.getNames()).to.be.an('Array').and.to.have.length.gt(0);
+  });
+
+  it('should be able to get default provider name', () => {
+    expect(fileManagement.providers.getDefaultName()).to.be.a('String').and.to.eql('default');
+  });
+
+  describe('Add a provider', () => {
+    it('should fail if there is no name provider', () => {
+      try {
+        fileManagement.providers.add();
+      } catch (e) {
+        expect(e.message).to.eq('name must be provided');
+      }
+    });
+
+    it('should fail if there is an existing provider already', () => {
+      const name = fileManagement.providers.getNames()[0];
+      try {
+        fileManagement.providers.add(name);
+      } catch (e) {
+        expect(e.message).to.eq(`Provider ${name} already exists`);
+      }
+    });
+
+    it('should fail if the name is OK but the class is not instance of BaseProvider', () => {
+      try {
+        fileManagement.providers.add('test provider', Error);
+      } catch (e) {
+        expect(e.message).to.eq('class must be instance of BaseProvider');
+      }
+    });
+
+    it('should add a provider', () => {
+      class TestProvider extends BaseProvider {
+        constructor(config) {
+          super(config);
+          this.name = 'test-provider';
+        }
+      }
+      const name = 'testProvider';
+      fileManagement.providers.add(name, TestProvider);
+
+      expect(fileManagement.providers.getNames()).to.include(name.toLowerCase());
+    });
+  });
+});
